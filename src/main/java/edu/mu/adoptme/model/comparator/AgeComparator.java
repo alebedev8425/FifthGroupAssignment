2c446ad0ded219c56f3adf2841be3e88ee00b96f
unknown <owenvenker@gmail.com>
2025-05-01 20:31:04 -0500

    Implemented AgeComparator and SpeciesComparator classes

--- src/main/java/edu/mu/adoptme/model/comparator/AgeComparator.java
+++ src/main/java/edu/mu/adoptme/model/comparator/AgeComparator.java
@@ -1,5 +1,18 @@
 package edu.mu.adoptme.model.comparator;
 
-public class AgeComparator {
+import edu.mu.adoptme.model.Pet;
+import java.util.Comparator;
+
+public class AgeComparator implements Comparator<Pet> {
+
+	/**
+	 * Allows comparison between pets based on age 
+	 * @return int 
+	 */
+	@Override
+	public int compare(Pet o1, Pet o2) {
+		// TODO Auto-generated method stub
+		return Integer.compare(o1.getAge(),o2.getAge());
+	}
 
 }
--- src/main/java/edu/mu/adoptme/model/comparator/SpeciesComparator.java
+++ src/main/java/edu/mu/adoptme/model/comparator/SpeciesComparator.java
@@ -1,5 +1,18 @@
 package edu.mu.adoptme.model.comparator;
 
-public class SpeciesComparator {
+import edu.mu.adoptme.model.Pet;
+import java.util.Comparator;
+
+public class SpeciesComparator implements Comparator<Pet>{
+
+	/**
+	 * Allows for comparison between Pets based alphabetically on species
+	 * @return int 
+	 */
+	@Override
+	public int compare(Pet o1, Pet o2) {
+		// TODO Auto-generated method stub
+		return o1.getSpecies().compareToIgnoreCase(o2.getSpecies());
+	}
 
 }
